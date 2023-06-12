2387969fc520782df3909474ed2bd8d971ff8e94
Clara Martins <cmcm11320@gmail.com>
2023-06-11 22:18:49 -0300

    Commit 3 - Replace Temp with Query

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ function gerarFaturaStr (fatura, pecas) {
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
   
+      function getPeca(apresentacao) {
+      return pecas[apresentacao.id];
+    }
+
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
+      // const peca = pecas[apre.id];
       let total = 0;
   
-      switch (peca.tipo) {
+      switch (getPeca(apre).tipo) {
       case "tragedia":
         total = 40000;
         if (apre.audiencia > 30) {
@@ -27,16 +31,16 @@ function gerarFaturaStr (fatura, pecas) {
         total += 300 * apre.audiencia;
         break;
       default:
-          throw new Error(`Peça desconhecia: ${peca.tipo}`);
+          throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
       }
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
@@ -65,6 +69,7 @@ function gerarFaturaStr (fatura, pecas) {
     }
     let total = calcularTotalApresentacao(apre, peca);
   }
+
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
